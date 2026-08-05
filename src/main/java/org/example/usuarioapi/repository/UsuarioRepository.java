ea35cd863c1900dd4bfd5066f732684ec0f0b906
Mike <michaelbue04@gmail.com>
2026-08-05 14:40:32 -0300

    feat: adiciona o repositorio do usuario

---
+++ src/main/java/org/example/usuarioapi/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package org.example.usuarioapi.repository;
+
+import org.example.usuarioapi.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
