8e83484aad9cfb289ff341e0066156f2c6ad882a
Mike <michaelbue04@gmail.com>
2026-08-05 14:24:57 -0300

    feat: adiciona o usuario

---
+++ src/main/java/org/example/usuarioapi/model/Usuario.java
@@ -0,0 +1,48 @@
+package org.example.usuarioapi.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Usuario {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cpf;
+    private String senha;
+
+    public Usuario(){
+        this.id = 0;
+        this.cpf = "";
+        this.senha = "";
+    }
+
+    public Usuario(Integer id, String cpf, String senha){
+        this.id = id;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
