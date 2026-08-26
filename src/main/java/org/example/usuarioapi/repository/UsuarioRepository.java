dc66ec1562732cddea45a49511507f2914fb7b77
Mike <michaelbue04@gmail.com>
2026-08-26 14:36:29 -0300

    feat: adiciona o service do usuario

--- src/main/java/org/example/usuarioapi/repository/UsuarioRepository.java
+++ src/main/java/org/example/usuarioapi/repository/UsuarioRepository.java
@@ -4,4 +4,5 @@ import org.example.usuarioapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
 }
